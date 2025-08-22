3aeea263cbbe9af00681b6fbc0023244e620be1a
MehediHasanRafi00 <mehedihasanrafi205@gmail.com>
2025-08-22 23:22:38 +0600

    add login functionality

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 // login button functionality
 
-document.getElementById("login-btn")
-.addEventListener("click", function(e){
-    e.preventDefault()
-    console.log('login button click');
-    console.log(e);
+document.getElementById("login-btn").addEventListener("click", function (e) {
+  e.preventDefault();
+  const mobileNumber = 12345678910;
+  const piNumber = 1234;
+
+  const mobileNumberValue = document.getElementById("mobile-number").value;
+  const mobileNumberValueConverted = parseInt(mobileNumberValue);
+
+  const pinNUmberValue = document.getElementById("pin-number").value;
+  const pinNumberValueConverted = parseInt(pinNUmberValue);
+
+
+
+  if (
+    mobileNumberValueConverted === mobileNumber &&
+    pinNumberValueConverted === piNumber
+  ) {
+    window.location.href = "./home.html";
+  } else {
+    alert("Invalid credentials");
+  }
 });
