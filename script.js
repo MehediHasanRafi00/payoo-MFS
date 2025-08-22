5d052a78d132c40c1bed4e92c8b2a842b6c55873
MehediHasanRafi00 <mehedihasanrafi205@gmail.com>
2025-08-22 22:59:16 +0600

    add login UI

---
+++ script.js
@@ -0,0 +1,8 @@
+// login button functionality
+
+document.getElementById("login-btn")
+.addEventListener("click", function(e){
+    e.preventDefault()
+    console.log('login button click');
+    console.log(e);
+});
