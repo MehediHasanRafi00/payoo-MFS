43c551154dd44ea635f265d094c4f0564c62c062
MehediHasanRafi00 <mehedihasanrafi205@gmail.com>
2025-08-23 03:57:38 +0600

    added add money section with functionality

---
+++ home.js
@@ -0,0 +1,35 @@
+// add money functionality
+
+const validPin = 1234;
+document
+  .getElementById("add-money-btn")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const bank = document.getElementById("bank").value;
+
+    const accountNumber = document.getElementById("account-number").value;
+
+    const amount = parseInt(document.getElementById("add-amount").value);
+
+    const pin = parseInt(document.getElementById("add-pin").value);
+
+    if (accountNumber.length < 11) {
+      alert("Please provide valid account number");
+      return;
+    }
+
+    if (pin !== validPin) {
+      alert("Please provide valid pin number");
+      return;
+    }
+
+    const availableBalance = parseInt(
+      document.getElementById("available-balance").innerText
+    );
+
+    const totalNewAvailableBalance = amount + availableBalance;
+
+    document.getElementById("available-balance").innerText =
+      totalNewAvailableBalance;
+  });
